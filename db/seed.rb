ac874da508efd66d3d26aa4b4dc217b66710f7f2
Damon Emde <damon.emde@gmail.com>
2015-04-12 11:58:41 -0400

    pushed the wrong docs before

--- db/seed.rb
+++ db/seed.rb
@@ -5,21 +5,29 @@ require_relative '../db/connection'
 Deck.destroy_all
 Card.destroy_all
 
-ruby_deck = Deck.create({category: 'Ruby'})
-data_types_deck = Deck.create({category: 'Data Types'})
+ruby_deck = Deck.create({name:'Ruby'})
+data_types_deck = Deck.create({name:'Data Types'})
+french_deck = Deck.create({name: 'French'})
 
-
-#card_front, card_back, guess_right, card_played
 data_types_deck.cards.create([
-    {card_front: 'This type is used typically for words or to print to the screen', card_back: 'string',
-    guess_right: false, card_played: false},
-    {card_front: 'This type is composed of whole numbers', card_back: 'integer',
-    guess_right: false, card_played: false}
+    {front: 'This type is used typically for words or to print to the screen', back: 'string',
+    score: 0, attempts: 0},
+    {front: 'This type is composed of whole numbers', back: 'integer',
+    score: 0, attempts: 0}
     ])
 
 ruby_deck.cards.create([
-    {card_front: 'Creator of the Ruby language', card_back: 'Matsumoto',
-    guess_right: false, card_played: false},
-    {card_front: 'This method will place an array in order', card_back: '.sort',
-    guess_right: false, card_played: false}
+    {front: 'Creator of the Ruby language', back: 'Matsumoto',
+    score: 0, attempts: 0},
+    {front: 'This method will place an array in order', back: '.sort',
+    score: 0, attempts: 0}
     ])
+
+french_deck.cards.create([
+    {front: 'Chat', back: 'Cat', score:0, attempts: 0},
+    {front: 'Chien', back: 'Dog', score:0, attempts: 0},
+    {front: 'Aller', back: 'to go', score:0, attempts: 0},
+    {front: 'Millefeuille', back: 'da bomb', score:0, attempts: 0},
+    {front: 'Renard', back: 'Fox', score:0, attempts: 0},
+    {front: "Je m'en fiche", back: "I don't care", score:0, attempts: 0}
+  ])
